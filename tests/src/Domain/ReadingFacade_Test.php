ef02c7a1ad9ecf6b049fda64415c7acd7c8cee51
Jeff Zohrab <jzohrab@gmail.com>
2022-12-22 21:19:53 -0600

    Fixing broken tests, reducing test clutter.

--- tests/src/Domain/ReadingFacade_Test.php
+++ tests/src/Domain/ReadingFacade_Test.php
@@ -14,7 +14,6 @@ final class ReadingFacade_Test extends DatabaseTestBase
     public function childSetUp(): void
     {
         $this->load_languages();
-        $this->load_spanish_words();
 
         $this->facade = new ReadingFacade(
             $this->reading_repo,
@@ -46,8 +45,14 @@ final class ReadingFacade_Test extends DatabaseTestBase
         $this->assertEquals(2, count($sentences), "reparsed");
     }
 
+
+    /**
+     * @group fixing
+     */
     public function test_mark_unknown_as_known_creates_words_and_updates_ti2s()
     {
+        DbHelpers::add_word($this->spanish->getLgID(), "lista", "lista", 3, 1);
+
         $content = "Hola tengo un gato.  No tengo una lista.\nElla tiene una bebida.";
         $t = $this->create_text("Hola", $content, $this->spanish);
 
@@ -57,15 +62,13 @@ final class ReadingFacade_Test extends DatabaseTestBase
         $expected = [
             "0; 1; Hola",
             "0; 3; tengo",
-            "1; 5; un gato",
             "0; 5; un",
             "0; 7; gato",
             "0; 10; No",
             "0; 12; tengo",
             "0; 14; una",
-            "2; 16; lista",
+            "1; 16; lista",
             "0; 19; Ella",
-            "3; 21; tiene una",
             "0; 21; tiene",
             "0; 23; una",
             "0; 25; bebida"
@@ -75,10 +78,7 @@ final class ReadingFacade_Test extends DatabaseTestBase
         $wordssql = "select wotext, wowordcount, wostatus from words order by woid";
         // DbHelpers::dumpTable($wordssql);
         $expected = [
-            "Un gato; 2; 1",
-            "lista; 1; 1",
-            "tiene una; 2; 1",
-            "listo; 1; 1"
+            "lista; 1; 3",
         ];
         DbHelpers::assertTableContains($wordssql, $expected, "initial words");
 
@@ -88,44 +88,23 @@ final class ReadingFacade_Test extends DatabaseTestBase
           order by ti2order, ti2wordcount desc";
         // DbHelpers::dumpTable($joinedti2s);
         $expected = [
-            "5; un gato; Un gato; 1",
-            "16; lista; lista; 1",
-            "21; tiene una; tiene una; 1"
+            "16; lista; lista; 3",
         ];
         DbHelpers::assertTableContains($joinedti2s, $expected, "initial ti2s mapped to words");
 
         $this->facade->mark_unknowns_as_known($t);
 
-        $expected = [
-            "Un gato; 2; 1",
-            "lista; 1; 1",
-            "tiene una; 2; 1",
-            "listo; 1; 1",
-            "bebida; 1; 99",
-            "ella; 1; 99",
-            "gato; 1; 99",
-            "hola; 1; 99",
-            "no; 1; 99",
-            "tengo; 1; 99",
-            "tiene; 1; 99",
-            "un; 1; 99",
-            "una; 1; 99"
-        ];
-        DbHelpers::assertTableContains($wordssql, $expected, "words created");
-
         // DbHelpers::dumpTable($joinedti2s);
         $expected = [
             "1; Hola; hola; 99",
             "3; tengo; tengo; 99",
-            "5; un gato; Un gato; 1",
             "5; un; un; 99",
             "7; gato; gato; 99",
             "10; No; no; 99",
             "12; tengo; tengo; 99",
             "14; una; una; 99",
-            "16; lista; lista; 1",
+            "16; lista; lista; 3",
             "19; Ella; ella; 99",
-            "21; tiene una; tiene una; 1",
             "21; tiene; tiene; 99",
             "23; una; una; 99",
             "25; bebida; bebida; 99"
@@ -140,9 +119,6 @@ final class ReadingFacade_Test extends DatabaseTestBase
     // hadn't been associated to the existing word record, and the
     // facade tried to create the _same_ word on marking this textitem2s as
     // well-known.
-    /**
-     * @group fixing
-     */
     public function test_mark_unknown_as_known_works_if_ti2_already_exists()
     {
         $content = "Hola tengo un perro.";
@@ -198,6 +174,8 @@ final class ReadingFacade_Test extends DatabaseTestBase
 
     public function test_update_status_creates_words_and_updates_ti2s()
     {
+        $this->load_spanish_words();
+
         $content = "Hola tengo un gato.  No tengo una lista.\nElla tiene una bebida.";
         $t = $this->create_text("Hola", $content, $this->spanish);
 
