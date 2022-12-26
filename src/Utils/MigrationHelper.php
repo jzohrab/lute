55ce0429f44777cf779f85bcfd6573e110364049
Jeff Zohrab <jzohrab@gmail.com>
2022-12-26 05:43:25 -0600

    Prevent blind migration of LWT db.

--- src/Utils/MigrationHelper.php
+++ src/Utils/MigrationHelper.php
@@ -28,6 +28,60 @@ class MigrationHelper {
         return $migration;
     }
 
+    /**
+     * Used by public/index.php to initiate and migrate the database.
+     * 
+     * Feels kind of messy, not sure where this belongs, but it will do
+     * until proven poor.  ... only tested manually.
+     *
+     * Returns [ messages, error string ]
+     */
+    public static function doSetup(): array {
+
+        $messages = [];
+        $error = null;
+        $newdbcreated = false;
+        try {
+            Connection::verifyConnectionParams();
+
+            if (MigrationHelper::isLearningWithTextsDb()) {
+                $args = [
+                    'dbname' => MigrationHelper::getOrThrow('DB_DATABASE'),
+                    'username' => MigrationHelper::getOrThrow('DB_USER')
+                ];
+                $error = MigrationHelper::renderError('will_not_migrate_lwt_automatically.html.twig', $args);
+                return [ $messages, $error ];
+            }
+
+            if (! Connection::databaseExists()) {
+                Connection::createBlankDatabase();
+                MigrationHelper::installBaseline();
+                $newdbcreated = true;
+                $messages[] = 'New database created.';
+            }
+            if (MigrationHelper::hasPendingMigrations()) {
+                MigrationHelper::runMigrations();
+                if (! $newdbcreated) {
+                    $messages[] = 'Database updated.';
+                }
+            }
+        }
+        catch (\Exception $e) {
+            $args = ['errors' => [ $e->getMessage() ]];
+            $error = DatabaseSetup::renderError('fatal_error.html.twig', $args);
+        }
+
+        return [ $messages, $error ];
+    }
+
+    private static function renderError($name, $args = []): string {
+        // ref https://twig.symfony.com/doc/2.x/api.html
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../../templates');
+        $twig = new \Twig\Environment($loader);
+        $template = $twig->load($name);
+        return $template->render($args);
+    }
+
     public static function hasPendingMigrations() {
         $migration = MigrationHelper::getMigrator();
         return count($migration->get_pending()) > 0;
@@ -79,6 +133,19 @@ class MigrationHelper {
                ->fetch_array();
         $c = intval($check['c']);
         return $c == 0;
+    }
+
+    public static function isLearningWithTextsDb() {
+        $dbname = MigrationHelper::getOrThrow('DB_DATABASE');
+        $sql = "select count(*) as c from information_schema.tables
+          where table_schema = '{$dbname}'
+          and table_name = '_lwtgeneral'";
+        $conn = Connection::getFromEnvironment();
+        $check = $conn
+               ->query($sql)
+               ->fetch_array();
+        $c = intval($check['c']);
+        return $c == 1;
     }
 
     public static function loadDemoData() {
