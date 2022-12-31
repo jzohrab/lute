1256148f5d6ca59dc65f41a8fce21f3f06b717ed
Jeff Zohrab <jzohrab@gmail.com>
2022-12-30 23:47:33 -0600

    Fix broken test.

--- tests/src/Utils/MigrationHelper_Test.php
+++ tests/src/Utils/MigrationHelper_Test.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../../src/Domain/Parser.php';
 require_once __DIR__ . '/../../DatabaseTestBase.php';
 
 use App\Utils\MigrationHelper;
+use App\Domain\Dictionary;
 
 /** Smoke tests only. */
 final class MigrationHelper_Test extends DatabaseTestBase
@@ -18,7 +19,8 @@ final class MigrationHelper_Test extends DatabaseTestBase
      * @group demo
      */
     public function test_smoke_can_load_demo_data() {
-        MigrationHelper::loadDemoData($this->language_repo, $this->text_repo, $this->term_repo);
+        $dict = new Dictionary($this->entity_manager);
+        MigrationHelper::loadDemoData($this->language_repo, $this->text_repo, $dict);
         $this->assertTrue(true, 'dummy');
         $t = $this->text_repo->find(1);
         $this->assertEquals($t->getTitle(), 'Tutorial', 'got tutorial, index link to /read/1 is good.');
