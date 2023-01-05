7871c618b7d970d269a50415ebceddaafe4d6763
Jeff Zohrab <jzohrab@gmail.com>
2023-01-05 09:49:41 -0600

    Add db setup composer scripts.

---
+++ db/setup.php
@@ -0,0 +1,24 @@
+<?php
+use Symfony\Component\Dotenv\Dotenv;
+use App\Utils\MigrationHelper;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+echo "\nSetting up {$_ENV['DB_DATABASE']}\n\n";
+[ $messages, $error ] = MigrationHelper::doSetup();
+
+echo "Done.\n";
+
+if ($error != null) {
+    echo "ERROR: \n" . $error . "\n";
+    throw new \Exception($error);
+}
+
+echo "Messages:\n";
+foreach ($messages as $m) {
+    echo $m . "\n";
+}
+
+?>
