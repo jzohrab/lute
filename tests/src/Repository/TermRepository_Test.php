612e46ccfa11e6ff30cc642daa7fafc9f0e2a03d
Jeff Zohrab <jzohrab@gmail.com>
2023-01-07 12:49:53 -0600

    Failing tests for new code.

---
+++ tests/src/Repository/TermRepository_Test.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../../db_helpers.php';
+require_once __DIR__ . '/../../DatabaseTestBase.php';
+
+use App\Entity\Term;
+
+final class TermRepository_Test extends DatabaseTestBase
+{
+
+    public function childSetUp() {
+        $this->load_languages();
+    }
+    
+    public function test_save()
+    {
+        DbHelpers::assertRecordcountEquals("select * from words", 0, "no terms");
+        $t = new Term($this->spanish, 'perro');
+        $this->term_repo->save($t, true);
+        DbHelpers::assertRecordcountEquals("select * from words", 1, "saved");
+    }
+
+    public function test_remove()
+    {
+        DbHelpers::assertRecordcountEquals("select * from words", 0, "no terms");
+        $t = new Term($this->spanish, 'perro');
+        $this->term_repo->save($t, true);
+        DbHelpers::assertRecordcountEquals("select * from words", 1, "saved");
+        $this->term_repo->remove($t, true);
+        DbHelpers::assertRecordcountEquals("select * from words", 0, "no terms, removed");
+    }
+
+}
