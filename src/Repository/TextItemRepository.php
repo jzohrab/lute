049b9fa73f4f4a91604aa62b238fc50b5a1696ab
Jeff Zohrab <jzohrab@gmail.com>
2023-01-17 17:58:18 -0600

    Remove dump() calls.

--- src/Repository/TextItemRepository.php
+++ src/Repository/TextItemRepository.php
@@ -338,20 +338,17 @@ where ti2woid = 0";
             $termmatches = [];
             if (count($allmatches) > 0)
                 $termmatches = $allmatches[1];
-            dump($termmatches);
+            // dump($termmatches);
             // Sample $termmatches data:
             // array(3) { [0]=> array(2) { [0]=> string(7) "Un gato", [1]=> int(2) }, ... }
 
             foreach($termmatches as $tm) {
                 $cnt = $this->get_term_count_before($string, $tm[1], $lang);
-                dump('this term = ' . $tm[0]);
-                dump('count of terms before this = ' . $cnt);
-                dump('sentence first pos = ' . $firstpos);
                 $pos = 2 * $cnt + (int) $firstpos;
                 if ($lang->isLgRemoveSpaces()) {
                     $pos = $cnt + (int) $firstpos;
                 }
-                dump('position to set for this = ' . $pos);
+                // dump('position to set for this = ' . $pos);
                 $txt = $tm[0];
 
                 $sql = "INSERT IGNORE INTO textitems2
@@ -360,8 +357,6 @@ where ti2woid = 0";
                 $params = array(
                     "iiiiiis",
                     $wid, $lid, $record['SeTxID'], $record['SeID'], $pos, $wordcount, $txt);
-                dump($sql);
-                dump($params);
                 $this->exec_sql($sql, $params);
 
             } // end foreach termmatches
