435f79264bd94c4aaea8fd313e301e6caaeef68e
Jeff Zohrab <jzohrab@gmail.com>
2023-01-30 12:52:35 -0600

    Test cases to implement.

--- tests/src/Domain/TextStatsCache_Test.php
+++ tests/src/Domain/TextStatsCache_Test.php
@@ -57,5 +57,77 @@ final class TextStatsCache_Test extends DatabaseTestBase
         DbHelpers::assertTableContains($sql, [ "11" ], "Updated");
     }
 
+    /** lastmaxstatuschanged is stored in the settings table **/
 
+    /**
+     * @group faststats
+     */
+    public function test_refresh_sets_lastmaxstatuschanged()
+    {
+        $t = new Text();
+        $t->setTitle("Hola.");
+        $t->setText("Hola tengo un gato.");
+        $t->setLanguage($this->spanish);
+        $this->text_repo->save($t, true);
+
+        TextStatsCache::refresh();
+        DbHelpers::assertRecordcountEquals("select * from settings where StKey = 'lastmaxstatuschanged'", 1, 'setting saved');
+        $sql = "select * from settings where StKey = 'lastmaxstatuschanged'
+          and StValue = (select convert(unix_timestamp(max(wostatuschanged)), char(40)) from words)";
+        DbHelpers::assertRecordcountEquals($sql, 1, 'correct setting value saved');
+    }
+
+    /**
+     * @group faststats
+     */
+    public function test_refresh_not_needed_if_lastmaxstatuschanged_setting_equals_max_wostatuschanged()
+    {
+        $t = new Text();
+        $t->setTitle("Hola.");
+        $t->setText("Hola tengo un gato.");
+        $t->setLanguage($this->spanish);
+        $this->text_repo->save($t, true);
+
+        TextStatsCache::refresh();
+
+        $sql = "update settings
+          set StValue = (select convert(unix_timestamp(max(wostatuschanged)), char(40)) from words)
+          where StKey = 'lastmaxstatuschanged'";
+        DbHelpers::exec_sql($sql);
+
+        $this->assertFalse(TextStatsCache::needs_refresh(), "not required");
+
+        $sql = "update settings
+          set StValue = '100'
+          where StKey = 'lastmaxstatuschanged'";
+        DbHelpers::exec_sql($sql);
+        $this->assertTrue(TextStatsCache::needs_refresh(), "required, last max changed is less than current");
+
+        $sql = "update settings
+          set StValue = (select convert(unix_timestamp(max(wostatuschanged) + 100), char(40)) from words)
+          where StKey = 'lastmaxstatuschanged'";
+        DbHelpers::exec_sql($sql);
+
+        $this->assertFalse(TextStatsCache::needs_refresh(), "not required, last max is greater than current");
+
+        DbHelpers::exec_sql('delete from words');
+        $this->assertFalse(TextStatsCache::needs_refresh(), "not required, no words, no stats");
+    }
+
+    /**
+     * @group faststats
+     */
+    public function test_refresh_needed_if_no_lastmaxstatuschanged()
+    {
+        $t = new Text();
+        $t->setTitle("Hola.");
+        $t->setText("Hola tengo un gato.");
+        $t->setLanguage($this->spanish);
+        $this->text_repo->save($t, true);
+
+        $sql = "delete from settings where StKey = 'lastmaxstatuschanged'";
+        DbHelpers::exec_sql($sql);
+
+        $this->assertTrue(TextStatsCache::needs_refresh(), "not required");
+    }
 }
