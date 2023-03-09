a25177e7b4a54105182aba1c73e7237e14ade1b3
16amattice <anthonymattice@gmail.com>
2023-03-09 16:08:52 -0600

    Fixes/Updates
    Added App_Env variable to app-init.sh.
    Change the location of the migration scripts to match current folder layout.
    Updated apache.conf to use latest practices.

--- docker/app-init.sh
+++ docker/app-init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# todo: replace prod with $1 and pass in from docker-compose.yml from APP_ENV
+# Check if the APP_ENV parameter is set or not
+if [ -z "$APP_ENV" ]; then
+  echo "APP_ENV parameter is missing!"
+  exit 1
+fi
 
 cd /var/www/html && \
-    composer run-script db:setup:prod && \
-    composer run-script db:migrate:prod
+    composer run-script db:setup:$APP_ENV && \
+    composer run-script db:migrate:$APP_ENV
