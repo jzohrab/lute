9aa6a228fa73a3a855b158d6d8a7cb985de8b954
Jeff Zohrab <jzohrab@gmail.com>
2023-03-10 14:18:08 -0600

    New domain classes for future work.

---
+++ src/Domain/RenderableCalculator.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Domain;
+
+class RenderableCalculator {
+
+    /**
+     * Returns array of matches in same format as preg_match or
+     * preg_match_all
+     * @param bool   $matchAll If true, execute preg_match_all, otherwise preg_match
+     * @param string $pattern  The pattern to search for, as a string.
+     * @param string $subject  The input string.
+     * @param int    $offset   The place from which to start the search (in bytes).
+     * @return array
+     *
+     * Ref https://stackoverflow.com/questions/1725227/preg-match-and-utf-8-in-php
+     */
+    private function pregMatchCapture($matchAll, $pattern, $subject, $offset = 0)
+    {
+        if ($offset != 0) { $offset = strlen(mb_substr($subject, 0, $offset)); }
+        
+        $matchInfo = array();
+        $method    = 'preg_match';
+        $flag      = PREG_OFFSET_CAPTURE;
+        if ($matchAll) {
+            $method .= '_all';
+        }
+
+        # var_dump([$method, $pattern, $subject, $matchInfo, $flag, $offset]);
+        $n = $method($pattern, $subject, $matchInfo, $flag, $offset);
+
+        $result = array();
+        if ($n !== 0 && !empty($matchInfo)) {
+            if (!$matchAll) {
+                $matchInfo = array($matchInfo);
+            }
+            foreach ($matchInfo as $matches) {
+                $positions = array();
+                foreach ($matches as $match) {
+                    $matchedText   = $match[0];
+                    $matchedLength = $match[1];
+                    // dump($subject);
+                    $positions[]   = array(
+                        $matchedText,
+                        mb_strlen(mb_strcut($subject, 0, $matchedLength))
+                    );
+                }
+                $result[] = $positions;
+            }
+            if (!$matchAll) {
+                $result = $result[0];
+            }
+        }
+        return $result;
+    }
+
+
+    private function get_count_before($string, $pos): int {
+        $beforesubstr = mb_substr($string, 0, $pos - 1, 'UTF-8');
+        $zws = mb_chr(0x200B);
+        $parts = explode($zws, $beforesubstr);
+        return count($parts);
+    }
+
+    private function get_all_textitems($words, $texttokens) {
+        $termmatches = [];
+
+        // Tokens must be contiguous and in order!
+        $cmp = function($a, $b) {
+            if ($a->TokOrder != $b->TokOrder) {
+                return ($a->TokOrder > $b->TokOrder) ? 1 : -1;
+            }
+        };
+        usort($texttokens, $cmp);
+        $prevtok = null;
+        foreach($texttokens as $tok) {
+            if ($prevtok != null) {
+                if ($prevtok->TokOrder != ($tok->TokOrder - 1)) {
+                    $mparts = [
+                        $prevtok->TokText, $prevtok->TokOrder,
+                        $tok->TokText, $tok->TokOrder
+                    ];
+                    throw new \Exception("bad token ordering: {implode('; ', $mparts)}");
+                }
+            }
+            $prevtok = $tok;
+        }
+
+        $firstTokOrder = $texttokens[0]->TokOrder;
+        $zws = mb_chr(0x200B);
+        $toktext = array_map(fn($t) => $t->TokText, $texttokens);
+        $subject = $zws . implode($zws, $toktext) . $zws;
+
+        foreach ($words as $w) {
+            $pattern = '/' . $zws . '('. $w->getTextLC() . ')' . $zws . '/ui';
+            $allmatches = $this->pregMatchCapture(true, $pattern, $subject, 0);
+            
+            if (count($allmatches) > 0) {
+                # echo "in loop\n";
+                # echo "===============\n";
+                # var_dump($allmatches);
+                # var_dump($allmatches[0]);
+                # echo "===============\n";
+                foreach ($allmatches[1] as $m) {
+                    # echo "------------\n";
+                    # var_dump($m);
+                    $result = new RenderableCandidate();
+                    $result->term = $w;
+                    $result->text = $m[0];
+
+                    // 1 is subtracted because the sentence has an extra $zws at the start,
+                    // so there is always an empty element at the start of the sentence.
+                    $result->pos = $firstTokOrder + $this->get_count_before($subject, $m[1]) - 1;
+                    $result->length = count(explode($zws, $w->getTextLC()));
+                    $result->isword = 1;
+                    # echo "------------\n";
+                    $termmatches[] = $result;
+                }
+            }
+            /*
+            else {
+                echo "no match for pattern $pattern \n";
+            }
+            */
+        }
+        
+        // Add originals
+        foreach ($texttokens as $tok) {
+            $result = new RenderableCandidate();
+            $result->term = null;
+            $result->text = $tok->TokText;
+            $result->pos = $tok->TokOrder;
+            $result->length = 1;  // Each thing parsed is 1 token!
+            $result->isword = $tok->TokIsWord;
+            $termmatches[] = $result;
+        }
+        return $termmatches;
+    }
+
+    private function calculate_hides(&$items) {
+        $isTerm = function($i) { return $i->term != null; };
+        $checkwords = array_filter($items, $isTerm);
+        // echo "checking words ----------\n";
+        // var_dump($checkwords);
+        // echo "------\n";
+        foreach ($checkwords as &$mw) {
+            $isContained = function($i) use ($mw) {
+                $contained = ($i->pos >= $mw->pos) && ($i->OrderEnd() <= $mw->OrderEnd());
+                $equivalent = ($i->pos == $mw->pos) && ($i->OrderEnd() == $mw->OrderEnd()) && ($i->getTermID() == $mw->getTermID());
+                return $contained && !$equivalent;
+            };
+            $hides = array_filter($items, $isContained);
+            // echo "checkword {$mw->text} has hides:\n";
+            // var_dump($hides);
+            // echo "end hides\n";
+            $mw->hides = $hides;
+            foreach ($hides as &$hidden) {
+                // echo "hiding " . $hidden->text . "\n";
+                $hidden->render = false;
+            }
+        }
+        return $items;
+    }
+
+
+    private function sort_by_order_and_tokencount($items): array
+    {
+        $cmp = function($a, $b) {
+            if ($a->pos != $b->pos) {
+                return ($a->pos > $b->pos) ? 1 : -1;
+            }
+            // Fallback: descending order, by token count.
+            return ($a->length > $b->length) ? -1 : 1;
+        };
+        usort($items, $cmp);
+        return $items;
+    }
+
+
+    public function main($words, $texttokens) {
+        $candidates = $this->get_all_textitems($words, $texttokens);
+        $candidates = $this->calculate_hides($candidates);
+        // echo "Term matches: ------------\n";
+        // foreach ($termmatches as $t) {
+        //     echo $t->term . ' => ' . $t->toString() . "\n";
+        // }
+        // echo "END Term matches: ------------\n";
+        // 
+        // echo "AFTER CALC ----------\n";
+        // foreach ($items as $i)
+        //     echo $i->toString() . "\n";
+        // echo "END AFTER CALC ----------\n";
+        $renderable = array_filter($candidates, fn($i) => $i->render);
+        $items = $this->sort_by_order_and_tokencount($renderable);
+        return $items;
+    }
+}
---
+++ tests/src/Domain/LongTextSplit_Test.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+use App\Domain\LongTextSplit;
+use App\Domain\RomanceLanguageParser;
+use App\Entity\Language;
+use PHPUnit\Framework\TestCase;
+
+final class LongTextSplit_Test extends TestCase
+{
+
+    public function test_getSentences_no_token_ok() {
+        $sentences = LongTextSplit::getSentences([]);
+        $this->assertEquals([], $sentences);
+    }
+
+    private function toks_to_string($tokens) {
+        $a = array_map(fn($t) => $t->token, $tokens);
+        return implode('', $a);
+    }
+    
+    public function test_one_sentence_returned() {
+        $eng = Language::makeEnglish();
+        $rlp = new RomanceLanguageParser();
+        $tokens = $rlp->getParsedTokens("Here is a dog.", $eng);
+        $sentences = LongTextSplit::getSentences($tokens);
+        $this->assertEquals(1, count($sentences), 'one sentence');
+        $s = $this->toks_to_string($sentences[0]);
+        $this->assertEquals("Here is a dog.", $s);
+    }
+
+    public function test_two_sentence_returned() {
+        $eng = Language::makeEnglish();
+        $rlp = new RomanceLanguageParser();
+        $tokens = $rlp->getParsedTokens("Here is a dog. Here is a cat.", $eng);
+        $sentences = LongTextSplit::getSentences($tokens);
+        $this->assertEquals(2, count($sentences), '2');
+        $s = $this->toks_to_string($sentences[0]);
+        $this->assertEquals("Here is a dog.\r", $s);
+        $s = $this->toks_to_string($sentences[1]);
+        $this->assertEquals(" Here is a cat.", $s, 'extra space at start');
+    }
+
+    private function scenario($s, $maxcount, $expected_groups) {
+        $eng = Language::makeEnglish();
+        $rlp = new RomanceLanguageParser();
+        $tokens = $rlp->getParsedTokens($s, $eng);
+
+        $groups = LongTextSplit::groups($tokens, $maxcount);
+
+        $gs = array_map(fn($g) => $this->toks_to_string($g), $groups);
+        $this->assertEquals(
+            implode("||", $gs),
+            implode("||", $expected_groups),
+            "groups for size $maxcount"
+        );
+    }
+
+    public function test_group_all_in_one_group() {
+        $text = "Here is a dog. Here is a cat.";
+
+        $this->scenario(
+            $text, 100,
+            [ "Here is a dog.\r Here is a cat." ]);
+
+        $this->scenario(
+            $text, 3,
+            [ "Here is a dog.\r", " Here is a cat." ]);
+
+        $this->scenario(
+            $text, 6,
+            [ "Here is a dog.\r", " Here is a cat." ]);
+
+        $this->scenario(
+            "Here is a dog. Here is a cat. Here is a thing.",
+            10,
+            [
+                "Here is a dog.\r Here is a cat.\r",
+                " Here is a thing."
+            ]);
+
+    }
+}
