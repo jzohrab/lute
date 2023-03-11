1df0e8e55e0163eacbdef23a229da21732b113fb
Jeff Zohrab <jzohrab@gmail.com>
2023-03-11 11:38:32 -0600

    Reuse tags2 for books.

--- db/migrations/20230311_005818_add_tagsforbooks_table.sql
+++ db/migrations/20230311_005818_add_tagsforbooks_table.sql
@@ -1,24 +1,9 @@
-CREATE TABLE `tagsforbooks` (
-  `T4BID` smallint unsigned NOT NULL AUTO_INCREMENT,
-  `T4BText` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `T4BComment` varchar(200) NOT NULL DEFAULT '',
-  PRIMARY KEY (`T4BID`),
-  UNIQUE KEY `T4BText` (`T4BText`)
+CREATE TABLE `booktags` (
+  `BtBkID` smallint unsigned NOT NULL,
+  `BtT2ID` smallint unsigned NOT NULL,
+  PRIMARY KEY (`BtBkID`,`BtT2ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
 
 
-insert into tagsforbooks (T4BID, T4BText, T4BComment)
-select T2ID, T2Text, T2Comment
-from tags;
-
-
-CREATE TABLE `booktags` (
-  `BtBkID` smallint unsigned NOT NULL,
-  `BtT4BID` smallint unsigned NOT NULL,
-  PRIMARY KEY (`BtTxID`,`TtT2ID`),
-  KEY `BtT4BID` (`BtT4BID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3
-
-
-insert into booktags (BtBkID, BtT4BID)
+insert into booktags (BtBkID, BtT2ID)
 select TtTxID, TtT2ID from texttags;
