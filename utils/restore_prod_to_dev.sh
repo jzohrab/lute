9b1c7adc1d1babf9497e1a68661553ed3ac6bfe7
Jeff Zohrab <jzohrab@gmail.com>
2023-03-12 19:39:41 -0600

    Note about db for restores.

--- utils/restore_prod_to_dev.sh
+++ utils/restore_prod_to_dev.sh
@@ -7,3 +7,9 @@ mysqladmin -u root -proot drop test_lute
 mysqladmin -u root -proot create test_lute
 
 mysql -u root -proot test_lute < dbexport.sql
+
+echo "=============================================="
+echo "Don't forget to run"
+echo
+echo "composer db:migrate:test"
+echo "=============================================="
