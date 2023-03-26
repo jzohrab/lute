8c70ab65b22565663d1f1ef00ee9682e92520873
Jeff Zohrab <jzohrab@gmail.com>
2023-03-26 14:39:56 -0700

    Ajax updating the backup page.

--- src/Controller/UtilsController.php
+++ src/Controller/UtilsController.php
@@ -6,8 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/utils')]
 class UtilsController extends AbstractController
@@ -17,7 +16,20 @@ class UtilsController extends AbstractController
     public function backup(): Response
     {
         return $this->render('utils/backup.html.twig', [
+            'backup_folder' => 'TODO:BACKUP some folder'
         ]);
+    }
+
+    #[Route('/do_backup', name: 'app_do_backup_index', methods: ['POST'])]
+    public function do_backup(): JsonResponse
+    {
+        try {
+            throw new \Exception('blah');
+            return $this->json('ok');
+        }
+        catch(\Exception $e) {
+            return new JsonResponse(array('errmsg' => $e->getMessage()), 500);
+        }
     }
 
 }
