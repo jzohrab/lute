1e407f200f5a78c6d081566a2929ea0fbf4c8717
Jeff Zohrab <jzohrab@gmail.com>
2023-04-08 22:53:35 -0700

    Remove obsolete comment only.

--- public/index.php
+++ public/index.php
@@ -24,12 +24,6 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-// TODO:sqlite handle misconfiguration
-// echo $_ENV['DATABASE_URL'];
-// echo "\n";
-// die('<html><body><h1>Hello! friendly message here</h1></body></html>');
-
-
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(
       explode(',', $trustedProxies),
