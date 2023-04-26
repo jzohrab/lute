7c0736a0a642aa2764fa64dcb66275ff196ad02a
Jeff Zohrab <jzohrab@gmail.com>
2023-04-25 22:48:35 -0700

    More test cases.

--- tests/src/Domain/TokenLocator_Test.php
+++ tests/src/Domain/TokenLocator_Test.php
@@ -10,11 +10,68 @@ class TokenLocator_Test extends TestCase
 
     public function test_scenario()
     {
+        $zws = mb_chr(0x200B);
+
+        // These cases are a bit hard to read ...
+        // an explanation:
+        //
+        // For each case,
+        // The first line (e.g. [ "a", "b", "c", "d" ]) is the list of tokens.
+        // The second is the word to find.
+        // Third is the expected result.
         $cases = [
+            // Finds b
             [ [ "a", "b", "c", "d" ],
               "b",
               [ [ "b", 1 ] ]
             ],
+
+            // Case doesn't matter
+            [ [ "A", "B", "C", "D" ],
+              "b",
+              [ [ "B", 1 ] ]  // The original case is returned
+            ],
+
+            [ [ "a", "b", "c", "d" ],
+              "B",
+              [ [ "b", 1 ] ]  // Original case returned.
+            ],
+
+            [ [ "a", "bb", "c", "d" ],
+              "B",
+              []  // No match
+            ],
+
+            [ [ "b", "b", "c", "d" ],
+              "b",
+              [ [ "b", 0 ], [ "b", 1 ] ]  // Found in multiple places.
+            ],
+
+            [ [ "b", "B", "b", "d" ],
+              "b{$zws}b",
+              [ [ "b{$zws}B", 0 ], [ "B{$zws}b", 1 ] ]  // multiword, found in multiple
+            ],
+
+            [ [ "b", "B", "c", "b", "b", "x", "b" ],
+              "b{$zws}b",
+              [ [ "b{$zws}B", 0 ], [ "b{$zws}b", 3 ] ]  // multiword, found in multiple
+            ],
+
+            [ [ "a", " ", "cat", " ", "here" ],
+              "cat",
+              [ [ "cat", 2 ] ]
+            ],
+
+            [ [ "a", " ", "CAT", " ", "here" ],
+              "cat",
+              [ [ "CAT", 2 ] ]
+            ],
+
+            [ [ "a", " ", "CAT", " ", "here" ],
+              "ca",
+              []  // no match
+            ],
+           
         ];
 
         foreach ($cases as $case) {
