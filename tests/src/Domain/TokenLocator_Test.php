123f48793d1896187beab888efe782beed1e36fb
Jeff Zohrab <jzohrab@gmail.com>
2023-04-25 22:39:14 -0700

    Prep for more tests.

--- tests/src/Domain/TokenLocator_Test.php
+++ tests/src/Domain/TokenLocator_Test.php
@@ -8,19 +8,29 @@ use PHPUnit\Framework\TestCase;
 class TokenLocator_Test extends TestCase
 {
 
-    public function test_create_book_creates_texts()
+    public function test_scenario()
     {
-        $tokens = [ "a", "b", "c", "d" ];
-        $word = "b";
+        $cases = [
+            [ [ "a", "b", "c", "d" ],
+              "b",
+              [ [ "b", 1 ] ]
+            ],
+        ];
 
-        $sentence = TokenLocator::make_string($tokens);
-        $word = TokenLocator::make_string($word);
+        foreach ($cases as $case) {
+            $tokens = $case[0];
+            $word = $case[1];
+            $expected = $case[2];
 
-        $actual = TokenLocator::locate($sentence, $word);
-        $expected = [ [ "b", 1 ] ];
-        // dump($actual);
-        // dump($expected);
-        $this->assertEquals($actual, $expected);
+            $sentence = TokenLocator::make_string($tokens);
+            $word = TokenLocator::make_string($word);
+            $actual = TokenLocator::locate($sentence, $word);
+            // dump($actual);
+            // dump($expected);
+
+            $msg = implode('', $tokens) . ' == ' . $word;
+            $this->assertEquals($actual, $expected, );
+        }
     }
 
 }
