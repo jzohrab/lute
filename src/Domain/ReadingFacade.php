dfeaddb0beca6be46ebaeb1bd9f4f49708b1f562
Jeff Zohrab <jzohrab@gmail.com>
2023-07-23 12:04:52 -0700

    Move method.

--- src/Domain/ReadingFacade.php
+++ src/Domain/ReadingFacade.php
@@ -7,7 +7,6 @@ use App\Entity\Text;
 use App\Entity\Term;
 use App\Entity\Language;
 use App\DTO\TermDTO;
-use App\DTO\TextToken;
 use App\Entity\Status;
 use App\Entity\Sentence;
 use App\Repository\TextRepository;
@@ -15,7 +14,6 @@ use App\Repository\BookRepository;
 use App\Repository\TermTagRepository;
 use App\Repository\TermRepository;
 use App\Domain\TermService;
-use App\Utils\Connection;
 
 class ReadingFacade {
 
@@ -42,68 +40,9 @@ class ReadingFacade {
     
     public function getSentences(Text $text)
     {
-        if ($text->getID() == null)
-            return [];
-
-        $tokens = $this->getTextTokens($text);
-        if (count($tokens) == 0) {
-            $text->getBook()->fullParse();
-            $tokens = $this->getTextTokens($text);
-        }
-
-        $terms = $this->term_repo->findTermsInText($text);
-
-        $makeRenderableSentence = function($sentenceNum, $tokens, $terms, $text) {
-            $setokens = array_filter($tokens, fn($t) => $t->TokSentenceNumber == $sentenceNum);
-            $renderable = RenderableCalculator::getRenderable($terms, $setokens);
-            $textitems = array_map(
-                fn($i) => $i->makeTextItem($sentenceNum, $text->getID(), $text->getLanguage()->getLgID()),
-                $renderable
-            );
-            return new RenderableSentence($sentenceNum, $textitems);
-        };
-
-        $usenums = array_map(fn($t) => $t->TokSentenceNumber, $tokens);
-        $renderableSentences = array_map(
-            fn($senum) => $makeRenderableSentence($senum, $tokens, $terms, $text),
-            array_unique($usenums)
-        );
-
-        return $renderableSentences;
+        return RenderableSentence::getSentences($text, $this->term_repo);
     }
 
-    private function getTextTokens(Text $t): array {
-        $textid = $t->getID();
-        if ($textid == null)
-            return [];
-
-        $sql = "select
-          TokSentenceNumber,
-          TokOrder,
-          TokIsWord,
-          TokText,
-          TokTextLC
-          from texttokens
-          where toktxid = $textid
-          order by TokSentenceNumber, TokOrder";
-
-        $conn = Connection::getFromEnvironment();
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
-        $ret = [];
-        foreach ($rows as $row) {
-            $tok = new TextToken();
-            $tok->TokSentenceNumber = intval($row['TokSentenceNumber']);
-            $tok->TokOrder = intval($row['TokOrder']);
-            $tok->TokIsWord = intval($row['TokIsWord']);
-            $tok->TokText = $row['TokText'];
-            $tok->TokTextLC = $row['TokTextLC'];
-            $ret[] = $tok;
-        }
-        return $ret;
-    }
 
     public function mark_read(Text $text) {
         $text->setReadDate(new DateTime("now"));
