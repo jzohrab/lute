81371115aa4d8dc49a2c4186bb117fc6f22954b0
Jeff Zohrab <jzohrab@gmail.com>
2023-09-22 21:12:53 -0700

    Remove unused dependency.

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Utils\Connection;
-use App\Utils\DemoDataLoader;
 use App\Utils\SqliteHelper;
 
 class DemoController extends AbstractController
