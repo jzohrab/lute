2f33ad5b43836b0ad753d73e77d20a2ac037c048
Jeff Zohrab <jzohrab@gmail.com>
2023-09-29 20:30:22 -0700

    Final slash.

--- src/Utils/Connection.php
+++ src/Utils/Connection.php
@@ -11,10 +11,12 @@ class Connection {
         // %kernel.project_dir% is a special symfony token.
         // https://symfony.com/doc/current/reference/configuration/kernel.html#project-directory
         // Replace it with the real path.
+        // The first two {$ds}.. are to get the correct relative path,
+        // and the final {$ds} replaces the '/' after %kernel.project_dir%.
         $ds = DIRECTORY_SEPARATOR;
-        $replacement = __DIR__ . "{$ds}..{$ds}..";
+        $replacement = __DIR__ . "{$ds}..{$ds}..{$ds}";
 
-        $d = str_replace('%kernel.project_dir%', $replacement, $_ENV['DB_FILENAME']);
+        $d = str_replace('%kernel.project_dir%/', $replacement, $_ENV['DB_FILENAME']);
 
         $dburl = "sqlite:///" . $d;
         if (array_key_exists('DB_WINDOWS_REPLACE_TRIPLE_SLASH', $_ENV)) {
