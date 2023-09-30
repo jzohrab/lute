82eb338708f80383b3526ac1988005d7ad12a4a6
Jeff Zohrab <jzohrab@gmail.com>
2023-09-30 15:02:13 -0700

    Add windows CI, check for no mecab.

---
+++ db/create_no_check.php
@@ -0,0 +1,17 @@
+<?php
+/*
+THIS FILE IS EXTREMELY DANGEROUS, BUT I ASSUME YOU KNOW WHAT YOU'RE DOING.
+
+This just hammers the existing db, replacing it with the baseline.
+
+Use this file at your peril.
+ */
+use App\Utils\SqliteHelper;
+use App\Utils\MyDotenv;
+
+$ds = DIRECTORY_SEPARATOR;
+require __DIR__ . $ds . implode($ds, ['..', 'vendor', 'autoload.php']);
+MyDotenv::boot(__DIR__ . $ds . implode($ds, ['..', '.env.test']));
+
+SqliteHelper::CreateDb();
+?>
