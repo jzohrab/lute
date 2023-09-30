d804a7a494ade81098da065690505a9ba683efa8
Jeff Zohrab <jzohrab@gmail.com>
2023-09-30 15:04:36 -0700

    Fix windows line ending issues.

--- src/Utils/Connection.php
+++ src/Utils/Connection.php
@@ -10,16 +10,24 @@ class Connection {
     public static function getFromEnvironment() {
         // %kernel.project_dir% is a special symfony token.
         // https://symfony.com/doc/current/reference/configuration/kernel.html#project-directory
-        $d = str_replace('%kernel.project_dir%', __DIR__ . '/../..', $_ENV['DATABASE_URL']);
+        // Replace it with the real path.
+        // The first two {$ds}.. are to get the correct relative path,
+        // and the final {$ds} replaces the '/' after %kernel.project_dir%.
+        $ds = DIRECTORY_SEPARATOR;
+        $replacement = __DIR__ . "{$ds}..{$ds}..{$ds}";
 
+        $d = str_replace('%kernel.project_dir%/', $replacement, $_ENV['DB_FILENAME']);
+
+        $dburl = "sqlite:///" . $d;
         if (array_key_exists('DB_WINDOWS_REPLACE_TRIPLE_SLASH', $_ENV)) {
             $replace = $_ENV['DB_WINDOWS_REPLACE_TRIPLE_SLASH'];
             $replace = strtolower($replace);
             if ($replace == 'yes') {
-                $d = str_replace('sqlite:///','sqlite:',$d);
+                $dburl = str_replace('sqlite:///','sqlite:',$dburl);
             }
         }
-        $dbh = new \PDO($d);
+        // dump('DB URL: ' . $dburl);  // TODO:remove
+        $dbh = new \PDO($dburl);
 
         // THIS IS EXTREMELY IMPORTANT!
         // Without this, foreign key cascade deletes do not work!
