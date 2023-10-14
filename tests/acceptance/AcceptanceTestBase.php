7f96eec975f816e747e32f58b8a86d90a38283cc
Jeff Zohrab <jzohrab@gmail.com>
2023-10-14 09:53:56 -0700

    Remove unused code.

--- tests/acceptance/AcceptanceTestBase.php
+++ tests/acceptance/AcceptanceTestBase.php
@@ -136,87 +136,10 @@ abstract class AcceptanceTestBase extends PantherTestCase
         $this->english = $english;
     }
 
-    public function load_spanish_words(): void
-    {
-        $zws = mb_chr(0x200B);
-        $terms = [ "Un{$zws} {$zws}gato", 'lista', "tiene{$zws} {$zws}una", 'listo' ];
-        $this->addTerms($this->spanish, $terms);
-    }
-
-    public function addTerms(Language $lang, $term_strings) {
-        $term_svc = new TermService($this->term_repo);
-        $arr = $term_strings;
-        if (is_string($term_strings))
-            $arr = [ $term_strings ];
-        $ret = [];
-        foreach ($arr as $t) {
-            $term = new Term($lang, $t);
-            $term_svc->add($term, true);
-            $ret[] = $term;
-        }
-        return $ret;
-    }
-
     public function make_text(string $title, string $text, Language $lang): Text {
         $b = Book::makeBook($title, $lang, $text);
         $this->book_repo->save($b, true);
         return $b->getTexts()[0];
-    }
-
-    public function save_term($text, $s) {
-        $textid = $text->getID();
-        $term_svc = new TermService($this->term_repo);
-        $facade = new ReadingFacade(
-            $this->term_repo,
-            $this->text_repo,
-            $this->book_repo,
-            $term_svc,
-            $this->termtag_repo
-        );
-        $dto = $facade->loadDTO($text->getLanguage(), $s);
-        $facade->saveDTO($dto);
-    }
-
-    private function get_renderable_textitems($text) {
-        $ret = [];
-
-        $term_svc = new TermService($this->term_repo);
-        $facade = new ReadingFacade(
-            $this->term_repo,
-            $this->text_repo,
-            $this->book_repo,
-            $term_svc,
-            $this->termtag_repo
-        );
-
-        $pps = $facade->getParagraphs($text);
-        foreach ($pps as $p) {
-            foreach ($p as $s) {
-                foreach ($s->renderable() as $ti) {
-                    $ret[] = $ti;
-                }
-            }
-        }
-        return $ret;
-    }
-
-    public function get_rendered_string($text, $imploder = '/', $overridestringize = null) {
-        $tis = $this->get_renderable_textitems($text);
-        $stringize = function($ti) {
-            $zws = mb_chr(0x200B);
-            $status = "({$ti->WoStatus})";
-            if ($status == '(0)' || $status == '()')
-                $status = '';
-            return str_replace($zws, '', "{$ti->Text}{$status}");
-        };
-        $usestringize = $overridestringize ?? $stringize;
-        $ss = array_map($usestringize, $tis);
-        return implode($imploder, $ss);
-    }
-
-    public function assert_rendered_text_equals($text, $expected) {
-        $s = $this->get_rendered_string($text);
-        $this->assertEquals($s, $expected);
     }
 
     public function clickLinkID($linkid) {
