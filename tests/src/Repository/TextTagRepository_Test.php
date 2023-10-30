894d0956c22c273f1d45b660e2493894c05fa629
Jeff Zohrab <jzohrab@gmail.com>
2023-10-29 17:45:48 -0700

    Skipping tests.

--- tests/src/Repository/TextTagRepository_Test.php
+++ tests/src/Repository/TextTagRepository_Test.php
@@ -17,20 +17,20 @@ final class TextTagRepository_Test extends DatabaseTestBase
         $this->texttag_repo->save($this->t, true);
     }
 
-    public function test_save()  // V3-port: TODO
+    public function test_save()  // V3-port: DONE skipping
     {
         $sql = "select T2Text, T2Comment from tags2";
         $expected = [ "Hola; Hola comment" ];
         DbHelpers::assertTableContains($sql, $expected);
     }
 
-    public function test_get_by_text()  // V3-port: TODO
+    public function test_get_by_text()  // V3-port: DONE skipping
     {
         $retrieved = $this->texttag_repo->findByText("Hola");
         $this->assertEquals($this->t->getId(), $retrieved->getId(), 'same item returned');
     }
 
-    public function test_get_by_text_returns_null_if_not_exact_match()  // V3-port: TODO
+    public function test_get_by_text_returns_null_if_not_exact_match()  // V3-port: DONE skipping
     {
         $retrieved = $this->texttag_repo->findByText("hola");
         $this->assertNull($retrieved, 'not exact text = no match');
